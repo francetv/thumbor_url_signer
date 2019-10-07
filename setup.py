5aca8ab4d9bd451e1066cefe48ec79191b79ba00
btl <bertrand.thill@francetv.fr>
2019-10-07 15:32:30 +0200

    fix

--- setup.py
+++ setup.py
@@ -1,33 +1,36 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
+import os
+from setuptools import setup, find_packages
 
-# Licensed under the MIT license:
-# http://www.opensource.org/licenses/mit-license
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    long_description = 'Thumbor signer adapters'
 
-from distutils.core import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 
 setup(
-    name = "thumbor_url_signer",
-    packages = ["url_signers"],
-    version = "1.2.3",
-    description = "Signer for Thumbor - Jeunesse Francetv Release",
-    author = "Bertrand Thill",
-    author_email = "bertrand.thill@francetv.fr",
-    keywords = ["thumbor", "images", "signer"],
-    license = 'MIT',
-    url = 'https://github.com/francetv/thumbor_url_signer',
-    classifiers = ['Development Status :: 4 - Beta',
-                   'Intended Audience :: Developers',
-                   'License :: OSI Approved :: MIT License',
-                   'Natural Language :: English',
-                   'Operating System :: POSIX :: Linux',
-                   'Programming Language :: Python :: 2.7',
-                   'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-                   'Topic :: Multimedia :: Graphics :: Presentation'
+    name="thumbor_url_signer",
+    version="0.0.6",
+    author="Bertrand THILL",
+    description=("Thumbor thumbor signer adapters - France.tv Release"),
+    license="MIT",
+    keywords="thumbor mongodb mongo",
+    url="https://github.com/francetv/thumbor_mongodb_storage_webp",
+    packages=[
+        'thumbor_url_signer',
+        'thumbor_url_signer.url_signer'
     ],
-    package_dir = {"url_signers": "url_signers"},
-    install_requires=['thumbor>=6.5.0','pymongo>=3.4.0'],
-    long_description = """\
-This module provide s
-"""
+    long_description=long_description,
+    classifiers=[
+        'Development Status ::4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+    ],
+    install_requires=[
+        'thumbor>=6.7.0'
+    ]
 )
